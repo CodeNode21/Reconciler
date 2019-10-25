8871418a7d03c0ab7044ba9f890996d6021774bd
CodeNode21 <mbednara@gmail.com>
2019-10-25 19:19:19 -0400

    CSV Splits

--- datahandlers/localstorage.js
+++ datahandlers/localstorage.js
@@ -1,6 +1,8 @@
 let firstLS = "LS first file";
 let secondLS = "LS second file";
 let thirdLS = "LS third file";
+//var csv require('csv');
+//var obj csv();
 
 $(document).ready(function(){
 	console.log("ready from localstorage file");
@@ -10,9 +12,7 @@ let LocalStorage = (file1, file2, file3) => {
 	console.log(file1.length);
 	
 	console.log(file1.isArray);
-	let arr = [];
-	//arr = file.map('"' = '');
-	console.log(arr);
+	parseCSV(file1);
 	//Handle file2 (Payables)
 	
 	//Handle file3 (Payments)
@@ -30,6 +30,7 @@ let LocalStorage = (file1, file2, file3) => {
 
 document.getElementById("runReportLS").addEventListener("click", lsButton, false);
 function lsButton() {
+
 	console.log("LS button clicked: Local Storage");
 	$("#reportArea").empty();
 	LocalStorage(file1, file2, file3);
@@ -37,7 +38,79 @@ function lsButton() {
 }
 })
 
-function parseCSV(file, delimiter, callback){
-	
+function parseCSV(file){
+	var allRows = file.split(/\r?\n|\r/);
+	console.log(allRows);
+	var table = '<table>';
+	for (var singleRow = 0; singleRow < allRows.length; singleRow++){
+		if(singleRow ===0) {
+			table += '<thead>';
+			table += '<tr>';
+		} else {
+			table += '<tr>';
+		}
+		var rowCells = allRows[singleRow].split(',');
+		for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+			if(singleRow === 0) {
+				table += '<th>';
+				table += rowCells[rowCell];
+				table += '</th>';
+			} else {
+				table += '<td>';
+				table += rowCells[rowCell];
+				table += '</td>';
+			}
+		}
+		if (singleRow === 0) {
+			table += '</tr>';
+			table += '</thead>';
+		} else {
+			table += '</tr>';
+		}
+	}
+	table += '</tbody>';
+	table += '</table>';
+	$("#reportArea").append(table);
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
